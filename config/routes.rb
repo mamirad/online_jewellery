9e3759a483d87a0473f1578573789a7c138ca6fc
aks-amir <muhammad.amir@activekeysolutions.com>
2020-12-31 14:09:51 +0500

    items scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :items do
+  	collection do
+
+  	end
+  	member do
+  	get:detail
+  	end
+  	end
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
