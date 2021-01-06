c65569e9c69170515117ae09dcee70e26b22ef13
mamirad <amirad2020@gmail.com>
2021-01-06 22:53:46 +0500

    make change

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 	enum status: [:active, :deactive]
-	enum is_warranty: [:yes,:no]
-	enum returnable: ['returnable', 'not returnable']
-	enum condition:[:new,:old]
+	# enum is_warranty: [:yes,:no]
+	# enum returnable: ['returnable', 'not returnable']
+	# enum condition:[:new,:old]
 end
